03a3fe229ab46319e036e06ed4c59b19892fd573
Riley <rileyborgard@gmail.com>
2016-03-24 18:31:31 -0400

    put sfml library in project
    now nobody should have to download their own copies of the library

--- Debug/src/main.d
+++ Debug/src/main.d
@@ -1,118 +1,118 @@
 src/main.o: ../src/main.cpp \
  C:\Users\Riley\Desktop\C++\Vermillion\include/MainScreen.h \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Texture.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Export.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Config.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Image.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Color.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Rect.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/Vector2.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/Vector2.inl \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Rect.inl \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/GlResource.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Export.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/RenderWindow.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/RenderTarget.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/View.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Transform.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/BlendMode.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/RenderStates.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/PrimitiveType.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Vertex.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/NonCopyable.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/Export.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Window.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/ContextSettings.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/VideoMode.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/WindowHandle.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/WindowStyle.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/Clock.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/Time.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/String.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Event.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Joystick.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Keyboard.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Mouse.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Texture.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Export.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Config.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Image.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Color.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Rect.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/Vector2.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/Vector2.inl \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Rect.inl \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/GlResource.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Export.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/RenderWindow.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/RenderTarget.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/View.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Transform.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/BlendMode.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/RenderStates.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/PrimitiveType.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Vertex.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/NonCopyable.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/Export.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Window.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/ContextSettings.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/VideoMode.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/WindowHandle.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/WindowStyle.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/Clock.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/Time.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/String.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Event.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Joystick.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Keyboard.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Mouse.hpp \
  C:\Users\Riley\Desktop\C++\Vermillion\include/TileMap.h \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Transformable.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/VertexArray.hpp \
- C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Drawable.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Transformable.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/VertexArray.hpp \
+ C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Drawable.hpp \
  C:\Users\Riley\Desktop\C++\Vermillion\include/Tile.h
 
 C:\Users\Riley\Desktop\C++\Vermillion\include/MainScreen.h:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Texture.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Texture.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Export.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Export.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Config.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Config.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Image.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Image.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Color.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Color.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Rect.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Rect.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/Vector2.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/Vector2.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/Vector2.inl:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/Vector2.inl:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Rect.inl:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Rect.inl:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/GlResource.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/GlResource.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Export.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Export.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/RenderWindow.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/RenderWindow.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/RenderTarget.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/RenderTarget.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/View.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/View.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Transform.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Transform.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/BlendMode.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/BlendMode.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/RenderStates.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/RenderStates.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/PrimitiveType.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/PrimitiveType.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Vertex.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Vertex.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/NonCopyable.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/NonCopyable.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/Export.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/Export.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Window.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Window.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/ContextSettings.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/ContextSettings.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/VideoMode.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/VideoMode.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/WindowHandle.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/WindowHandle.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/WindowStyle.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/WindowStyle.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/Clock.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/Clock.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/Time.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/Time.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/System/String.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/System/String.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Event.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Event.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Joystick.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Joystick.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Keyboard.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Keyboard.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Window/Mouse.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Window/Mouse.hpp:
 
 C:\Users\Riley\Desktop\C++\Vermillion\include/TileMap.h:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Transformable.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Transformable.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/VertexArray.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/VertexArray.hpp:
 
-C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include/SFML/Graphics/Drawable.hpp:
+C:\Users\Riley\Desktop\C++\Vermillion\include/SFML/Graphics/Drawable.hpp:
 
 C:\Users\Riley\Desktop\C++\Vermillion\include/Tile.h:
--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -26,7 +26,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -I"C:\Users\Riley\Desktop\C++\Vermillion\include" -I"C:\Users\Riley\Desktop\archives\libraries\SFML-2.0\include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I"C:\Users\Riley\Desktop\C++\Vermillion\include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
